4d12b9127912d3466531919217feed54676d4819
Marek Szmit <kosmonauta120@gmail.com>
2017-05-26 13:14:33 +0200

    Init

---
+++ DialogueCreator/DialogueCreator.py
@@ -0,0 +1,49 @@
+InputFileName = input("Insert path to input file: ")
+OutputFileName = InputFileName[:InputFileName.find(".txt")] + ".xml"
+
+try:
+    InputFile = open(InputFileName, "r")
+except IOError:
+    print("Could not open file")
+    exit()
+InputStr = InputFile.read()
+InputFile.close()
+
+OutputStr = "<ConditionalActions>" + '\n'
+
+targetComp = ""
+k=0
+
+temp = InputStr.find('\n')
+if(temp<0): exit()
+targetComp = InputStr[0:temp]
+k = temp+1
+
+delay = ""
+Name = ""
+while k<len(InputStr):
+	temp = InputStr[k:].find(' ')
+	if(temp<0):
+		break
+	delay = InputStr[k:(temp+k)]
+	
+	k = temp + k
+	
+	temp = InputStr[k:].find(':')
+	if(temp<0):
+		break
+	Name = InputStr[k:(temp+k)]
+	
+	k = temp+k+1
+	
+	temp = InputStr[k:].find('\n')
+	if(temp<0):
+		break
+	OutputStr += "<AddIRCMessage Author=\"{}\" TargetComp=\"{}\" Delay=\"{}\">".format(Name, targetComp, delay) + InputStr[k:temp+k] + "</AddIRCMessage>" + '\n'
+	k = temp+k+1
+	
+OutputStr += "<\ConditionalActions>"
+
+OutputFile = open(OutputFileName, "w")
+OutputFile.write(OutputStr)
+OutputFile.close()
